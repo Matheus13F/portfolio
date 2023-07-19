876cf4b6405ec7cc7dc162928493257a4d1d02d8
matheus13f <matheus-goes13@hotmail.com>
2023-07-19 00:39:07 -0300

    fetch with case

--- src/utils/fetchSocials.ts
+++ src/utils/fetchSocials.ts
@@ -2,7 +2,7 @@ export async function fetchSocials() {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/getSocials`,
     {
-      cache: "no-store",
+      cache: "default",
     }
   );
   const data = await res.json();
