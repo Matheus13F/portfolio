5cbd6bd84a768c8c9090d402b7557fb3be4044cc
matheus13f <matheus-goes13@hotmail.com>
2023-07-20 00:20:47 -0300

    added google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,20 @@
+import { Metadata } from "next";
 import "./globals.css";
 
 import { Poppins } from "next/font/google";
+import { Suspense } from "react";
+import Analytics from "@/components/Analytics";
 
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["300", "500", "600", "700", "800", "900"],
 });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Matheus",
   description: "Matheus Goes | Desenvolvedor",
   icons: {
-    icon: "logo.png",
+    icon: "/logo.png",
   },
 };
 
@@ -22,7 +25,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${poppins.className} scroll-smooth`}>
-      <body className="bg-[#f1f1f1]">{children}</body>
+      <body className="bg-[#f1f1f1]">
+        <Suspense>
+          <Analytics />
+        </Suspense>
+        {children}
+      </body>
     </html>
   );
 }
